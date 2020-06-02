2db3cf0188122628d2c35a3879dc1486ea608daf
yuyan2do <yuyan2do@gmail.com>
2020-06-02 09:35:41 -0700

    adapt_attention

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -148,6 +148,7 @@ class MultiheadAttention(nn.Module):
             # A workaround for quantization to work. Otherwise JIT compilation
             # treats bias in linear module as method.
             and not torch.jit.is_scripting()
+            and False
         ):
             assert key is not None and value is not None
             return F.multi_head_attention_forward(
@@ -339,7 +340,7 @@ class MultiheadAttention(nn.Module):
         if before_softmax:
             return attn_weights, v
 
-        attn_weights_float = utils.softmax(
+        attn_weights_float = utils.adapt_softmax(
             attn_weights, dim=-1, onnx_trace=self.onnx_trace
         )
         attn_weights = attn_weights_float.type_as(attn_weights)
