db6783e47958fe424810d715ad22487a89290721
yuyan2do <yuyan2do@gmail.com>
2020-06-15 22:11:52 -0700

    fix

--- fairseq/utils.py
+++ fairseq/utils.py
@@ -539,5 +539,6 @@ class attention_weight_adjust(torch.autograd.Function):
         # pydevd.settrace(suspend=False, trace_only_current_thread=True)
         input, = ctx.saved_tensors
         grad_input = grad_output.clone()
-        grad_input = torch.true_divide(grad_input, torch.clamp(input.sum(dim=-2, keepdim=True), min=1))
+        # grad_input = torch.true_divide(grad_input, torch.clamp(input.sum(dim=-2, keepdim=True), min=1))
+        grad_input = grad_input / torch.clamp(input.sum(dim=-2, keepdim=True), min=1)
         return grad_input
