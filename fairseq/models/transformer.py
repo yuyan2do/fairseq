b41f8990ab8cea7ded2b6247a6bd5701f01ac413
yuyan2do <yuyan2do@gmail.com>
2020-07-06 22:55:57 -0700

    div 3 and 2

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -684,7 +684,7 @@ class TransformerDecoder(FairseqIncrementalDecoder):
         )
         if not features_only:
             x = self.output_layer(x)
-        x = x / (math.sqrt(self.args.encoder_embed_dim) / 3)
+        x = x / (math.sqrt(self.args.encoder_embed_dim) / 2)
         return x, extra
 
     def extract_features(
