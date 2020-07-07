e656407b0303ae4c9c66687fa3ea2401dbed1826
yuyan2do <yuyan2do@gmail.com>
2020-07-07 08:59:54 -0700

    2 and sqrt(2)

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -223,8 +223,8 @@ class TransformerModel(FairseqEncoderDecoderModel):
         encoder = cls.build_encoder(args, src_dict, encoder_embed_tokens)
         decoder = cls.build_decoder(args, tgt_dict, decoder_embed_tokens)
         with torch.no_grad():
-            tokens_scale = math.sqrt(args.encoder_embed_dim) / 3
-            positions_scale = tokens_scale / 3
+            tokens_scale = math.sqrt(args.encoder_embed_dim) / 2
+            positions_scale = tokens_scale / math.sqrt(2)
             encoder.embed_tokens.weight.mul_(tokens_scale)
             encoder.embed_positions.weight.mul_(positions_scale)
             decoder.embed_positions.weight.mul_(positions_scale)
